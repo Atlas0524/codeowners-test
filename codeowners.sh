c62db6c4563053348eefc391f2bf1910dfb71c47
David Tolley <90058832+Atlas0524@users.noreply.github.com>
2023-05-05 21:44:34 -0600

    Update codeowners.sh

--- codeowners.sh
+++ codeowners.sh
@@ -10,5 +10,5 @@ docker cp . $CONTAINER_NAME:$CONTAINER_APP_DIR
 cd $CODEOWNERS_TOOL_PATH
 docker-compose exec codeowners npm install
 docker-compose exec codeowners node index.js audit -r /codeowners -u
-docker cp $CONTAINER_NAME:/codeowners/tools/codeowners/codeowner-audit .
+docker cp $CONTAINER_NAME:/codeowners/codeowner-audit .
 docker-compose down
