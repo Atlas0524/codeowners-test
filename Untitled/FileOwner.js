d4db5d2bf2fc8fed88f7323c304a2ec18150c3e2
David Tolley <davidtolley@Davids-MacBook-Pro.local>
2023-05-05 21:49:00 -0600

    clean up

--- Untitled/FileOwner.js
+++
@@ -1,7 +0,0 @@
-module.exports = class FileOwner {
- constructor(fileName, fileOwned, fileOwner) {
-    this.fileName = fileName;
-    this.fileOwned = fileOwned;
-    this.fileOwner = fileOwner;
-  }
-}
